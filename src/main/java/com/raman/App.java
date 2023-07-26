eb0e23a5759074d8d5fcf684f301a06bad68dc99
RexRK <kraman938@gmail.com>
2023-07-26 18:31:57 +0530

    Interface Created

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -1,13 +1,55 @@
 package com.raman;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.Console;
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner in = new Scanner(System.in);
+        
+        
+        //Main Interface
+        byte option;
+        do {
+            System.out.println("<<<<<<<< GUESS THE NUMBER >>>>>>>>");
+            
+            System.out.println("    1. User Picks a Number !");
+            System.out.println("    2. Computer picks a Number !");
+            System.out.println("    3. How to Play ?");
+            System.out.println("    4. Quit !");
+            
+            System.out.print("Select an option : ");
+            option = in.nextByte();
+
+            switch(option) {
+                case 1 : userPlays();
+                case 2 : computerPlays();
+                case 3 : instruction();
+                case 4 : break;
+            }
+
+        } while (option != 4);
+
     }
+    
+    public static void userPlays() {
+
+    }
+    public static void computerPlays() {
+
+    }
+
+    public static void instruction() {
+        System.out.println("\nHOW TO PLAY ???");
+        System.out.println((char)4 + " The Computer will pick a secret number.");
+        System.out.println((char)4 + " You guess what number it is.");
+        System.out.println((char)4 + " If your guess is too high or too low, It will give you a hint.");
+        System.out.println((char)4 + " See how many turns it takes you to win !");
+        System.out.println();
+    }
+
+
+
 }
